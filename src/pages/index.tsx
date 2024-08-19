ad18a33b38ce6e7a46db5be7ea4e9e31fae6d179
Bruno Dias <brunodias40@gmail.com>
2024-08-19 11:53:24 -0300

    feat: add API endpoints for fetching products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 
 export default function Home() {
   return (
@@ -11,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>Hello</h1>
+        <h1>Hello World!</h1>
       </div>
     </>
   );
