bae694a469775feccb4e0cdaa60fdb566ec24722
Bruno Dias <brunodias40@gmail.com>
2024-08-20 14:32:39 -0300

    chore: add redirects and image domains to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: ["store.storeimages.cdn-apple.com"],
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  pageExtensions: [
+    "page.tsx",
+    "page.ts",
+    "page.jsx",
+    "page.js",
+    "api.tsx",
+    "api.ts",
+  ],
 };
 
 export default nextConfig;
